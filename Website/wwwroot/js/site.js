bd8f5e09c9350c265227f1374e69f253b7fc705d
Thabang Teddy Morwasetla <teddymorwasetla@gmail.com>
2025-04-20 01:31:20 +0200

    Added dark mode toggle

--- Website/wwwroot/js/site.js
+++ Website/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+$('.toggle-dark-mode').on('click', function () {
+    $('html').toggleClass('dark');
+});
